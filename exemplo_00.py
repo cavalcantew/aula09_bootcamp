1c021f9a71e96aea453205d54f9e86c3ac8b020f
Wagner Cavalcante <wagnerc@gmail.com>
2024-04-05 19:02:00 -0300

    update

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,7 +1,10 @@
 from log import log_decorator
+from timer import time_measure_decorator
+import time
 
-@log_decorator
+@time_measure_decorator
 def soma(x, y):
+    time.sleep(5)
     return x + y
 
 soma(2,5)
